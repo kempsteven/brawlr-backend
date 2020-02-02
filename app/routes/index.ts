b0893bbc9de00059f7b341ff8d4da9839d0e0a5a
kemp sayson <kemp.taisondigital@gmail.com>
2020-02-02 10:50:14 +0800

    Added route for continuius heroku wake

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,11 +1,16 @@
 import { userRoutes } from './user'
 import { matchRoutes } from './match'
 import { messageRoutes } from './message'
+import { Request, Response, NextFunction } from 'express'
 
 export class Routes {
     public routes (app : any) : void {
         userRoutes.setRoutes(app)
         matchRoutes.setRoutes(app)
         messageRoutes.setRoutes(app)
+
+        app.route('/').get((req: Request, res: Response) => {
+            res.status(200).send({ message: 'Hallo Kaffeine' })
+        })
     }
 }
